ce2639a6ee309b10ad4e6ade7fdd6b58fe03b47e
Stefan Laubenberger <slaubenberger@crosstales.com>
2024-06-08 17:28:46 +0200

    XML serializable dictionary added

---
+++ BogaNet.Common/Util/XMLSerializableDictionary.cs
@@ -0,0 +1,149 @@
+﻿using System.Xml.Serialization;
+using System.Runtime.Serialization;
+using System.Xml.Schema;
+using System.Xml;
+
+namespace BogaNet.Util;
+
+/// <summary>
+/// Serializable Dictionary-class for XML.
+/// </summary>
+[System.Serializable]
+public class XMLSerializableDictionary<TKey, TVal> : Dictionary<TKey, TVal>, IXmlSerializable, ISerializable where TKey : notnull
+{
+   #region Variables
+
+   //private const string DictionaryNodeName = "Dictionary";
+   private const string ItemNodeName = "Item";
+   private const string KeyNodeName = "Key";
+   private const string ValueNodeName = "Value";
+
+   private XmlSerializer? keySerializer;
+   private XmlSerializer? valueSerializer;
+
+   #endregion
+
+
+   #region Constructors
+
+   public XMLSerializableDictionary()
+   {
+      //empty
+   }
+
+   public XMLSerializableDictionary(IDictionary<TKey, TVal> dictionary) : base(dictionary)
+   {
+      //empty
+   }
+
+   public XMLSerializableDictionary(IEqualityComparer<TKey> comparer) : base(comparer)
+   {
+      //empty
+   }
+
+   public XMLSerializableDictionary(int capacity) : base(capacity)
+   {
+      //empty
+   }
+
+   public XMLSerializableDictionary(IDictionary<TKey, TVal> dictionary, IEqualityComparer<TKey> comparer) : base(dictionary, comparer)
+   {
+      //empty
+   }
+
+   public XMLSerializableDictionary(int capacity, IEqualityComparer<TKey> comparer) : base(capacity, comparer)
+   {
+      //empty
+   }
+
+   #endregion
+
+
+   #region ISerializable Members
+
+   protected XMLSerializableDictionary(SerializationInfo info, StreamingContext context)
+   {
+      int itemCount = info.GetInt32("ItemCount");
+      for (int ii = 0; ii < itemCount; ii++)
+      {
+         System.Collections.Generic.KeyValuePair<TKey, TVal> kvp = (KeyValuePair<TKey, TVal>)info.GetValue($"Item{ii}", typeof(KeyValuePair<TKey, TVal>))!;
+         Add(kvp.Key, kvp.Value);
+      }
+   }
+
+   void ISerializable.GetObjectData(SerializationInfo info, StreamingContext context)
+   {
+      info.AddValue("ItemCount", Count);
+      int itemIdx = 0;
+      foreach (KeyValuePair<TKey, TVal> kvp in this)
+      {
+         info.AddValue($"Item{itemIdx}", kvp, typeof(System.Collections.Generic.KeyValuePair<TKey, TVal>));
+         itemIdx++;
+      }
+   }
+
+   #endregion
+
+
+   #region IXmlSerializable Members
+
+   void IXmlSerializable.WriteXml(XmlWriter writer)
+   {
+      foreach (KeyValuePair<TKey, TVal> kvp in this)
+      {
+         writer.WriteStartElement(ItemNodeName);
+         writer.WriteStartElement(KeyNodeName);
+         KeySerializer.Serialize(writer, kvp.Key);
+         writer.WriteEndElement();
+         writer.WriteStartElement(ValueNodeName);
+         ValueSerializer.Serialize(writer, kvp.Value);
+         writer.WriteEndElement();
+         writer.WriteEndElement();
+      }
+   }
+
+   void IXmlSerializable.ReadXml(XmlReader? reader)
+   {
+      if (reader == null || reader.IsEmptyElement)
+         return;
+
+      if (!reader.Read())
+         throw new XmlException("Error in Deserialization of Dictionary");
+
+      while (reader.NodeType != System.Xml.XmlNodeType.EndElement)
+      {
+         reader.ReadStartElement(ItemNodeName);
+         reader.ReadStartElement(KeyNodeName);
+         if (KeySerializer != null)
+         {
+            TKey key = (TKey)KeySerializer.Deserialize(reader)!;
+            reader.ReadEndElement();
+            reader.ReadStartElement(ValueNodeName);
+            TVal value = (TVal)ValueSerializer.Deserialize(reader)!;
+            reader.ReadEndElement();
+            reader.ReadEndElement();
+            Add(key, value);
+         }
+
+         reader.MoveToContent();
+      }
+
+      reader.ReadEndElement();
+   }
+
+   XmlSchema? IXmlSerializable.GetSchema()
+   {
+      return null;
+   }
+
+   #endregion
+
+
+   #region Private Properties
+
+   private XmlSerializer ValueSerializer => valueSerializer ??= new XmlSerializer(typeof(TVal));
+
+   private XmlSerializer KeySerializer => keySerializer ??= new XmlSerializer(typeof(TKey));
+
+   #endregion
+}
