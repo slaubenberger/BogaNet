f186c126038b04df81e64c10b223b0ca5797f9ec
Stefan Laubenberger <laubenberger@gmail.com>
2024-07-03 16:08:21 +0200

    BNSpecifyKind added

--- BogaNet.Common/Extension/DateTimeExtension.cs
+++ BogaNet.Common/Extension/DateTimeExtension.cs
@@ -31,18 +31,18 @@ public static class DateTimeExtension //NUnit
    }
 
    /// <summary>
-   /// Converts a DateTime from a given time zone to utc.
+   /// Converts a DateTime from a given time zone to UTC.
    /// </summary>
    /// <param name="date">Date to convert</param>
    /// <param name="fromTZ">Origin time zone (optional, default: local)</param>
-   /// <returns>Converted DateTime as utc</returns>
+   /// <returns>Converted DateTime as UTC</returns>
    public static DateTime BNConvertTimeZoneToUtc(this DateTime date, TimeZoneInfo? fromTZ = null)
    {
       return TimeZoneInfo.ConvertTimeToUtc(date, fromTZ ?? (date.Kind == DateTimeKind.Utc ? TimeZoneInfo.Utc : TimeZoneInfo.Local));
    }
 
    /// <summary>
-   /// Converts a DateTime from utc to a given time zone.
+   /// Converts a DateTime from UTC/local to a given time zone.
    /// </summary>
    /// <param name="date">Date to convert</param>
    /// <param name="toTZ">Destination time zone (optional, default: local)</param>
@@ -50,6 +50,17 @@ public static class DateTimeExtension //NUnit
    public static DateTime BNConvertToTimeZone(this DateTime date, TimeZoneInfo? toTZ = null)
    {
       return date.Kind == DateTimeKind.Utc ? TimeZoneInfo.ConvertTimeFromUtc(date, toTZ ?? TimeZoneInfo.Local) : TimeZoneInfo.ConvertTime(date, toTZ ?? TimeZoneInfo.Local);
+   }
+
+   /// <summary>
+   /// Specifies a kind (UTC/local) for a given DateTime.
+   /// </summary>
+   /// <param name="date">Date to specify the kind</param>
+   /// <param name="kind">Kind for the DateTime (optional, default: UTC)</param>
+   /// <returns>DateTime with the specified kind</returns>
+   public static DateTime BNSpecifyKind(this DateTime date, DateTimeKind? kind = null)
+   {
+      return DateTime.SpecifyKind(date, kind ?? DateTimeKind.Utc);
    }
 
    #endregion
