fb207e10cc2639c3bea3512d42f77b357d3b5485
Stefan Laubenberger <laubenberger@gmail.com>
2024-08-09 00:22:44 +0200

    ObservableCollectionWithRange added

---
+++ BogaNet.Common/Util/ObservableCollectionWithRange.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace BogaNet.Util;
+
+/// <summary>
+/// Specialized ObservableCollection with AddRange.
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class ObservableCollectionWithRange<T> : ObservableCollection<T>
+{
+    /// <summary>
+    /// Adds all elements of the specified collection to the end of the collection.
+    /// </summary>
+    /// <param name="collection">Collection with the elements</param>
+    public void AddRange(IEnumerable<T> collection)
+    {
+        CheckReentrancy();
+
+        foreach (var item in collection)
+        {
+            Items.Add(item);
+        }
+
+        OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+    }
+}
